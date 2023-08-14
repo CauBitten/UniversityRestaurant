5a9b1f4f03bf0faee3a9fdcd070d70c0318c672d
Cauã Bittencourt <97898504+CauBitten@users.noreply.github.com>
2023-08-14 00:08:36 -0300

    Delete RestauranteUniversitario directory

--- RestauranteUniversitario/src/negocio/beans/Gerente.java
+++
@@ -1,8 +0,0 @@
-public class Gerente extends Usuario {
-
-    Gerente(String senha, String login, String email,
-            String nome, long cpf, boolean ativado) {
-
-        super(senha, login, email, nome, cpf, ativado);
-    }
-}
