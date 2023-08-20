fb0acdea82049c7a05cd572dc4bbe8f8221d1250
Higor Matheus <higormatheus1618@gmail.com>
2023-08-20 08:40:57 -0300

    "Atualizando Screen Manager"

--- RestauranteUniversitario/src/main/java/view/ScreenManager.java
+++ RestauranteUniversitario/src/main/java/view/ScreenManager.java
@@ -1,4 +1,43 @@
 package view;
 
+import gui.login.ControllerLoginPage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ScreenManager {
+    private static ScreenManager instance;
+    private Stage primaryStage;
+
+    private Scene loginScene;
+    private ControllerLoginPage loginController;
+
+    private ScreenManager() {
+        this.initialize();
+    }
+
+    public static ScreenManager getInstance() {
+        if (instance == null) {
+            instance = new ScreenManager();
+        }
+
+        return instance;
+    }
+
+    private void initialize() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+
+            GridPane loginPane = fxmlLoader.load(getClass()
+                    .getResource("/login/telaDeLogin.fxml").openStream());
+            this.loginScene = new Scene(loginPane);
+            this.loginController = (ControllerLoginPage) fxmlLoader.getController();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
