e962b5d64a3e9276e41858491d90c00713e07521
Cauã Bittencourt <caua.fb@gmail.com>
2023-08-22 10:58:02 -0300

    Tela Cadastro Usuario Atualizada

--- RestauranteUniversitario/src/main/java/gui/gerente/ControllerListarUsuarios.java
+++ RestauranteUniversitario/src/main/java/gui/gerente/ControllerListarUsuarios.java
@@ -1,15 +1,30 @@
 package gui.gerente;
 
+import dados.IRepositorioUsuario;
+import dados.RepositorioUsuario;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import negocio.beans.Usuario;
 import view.ScreenManager;
 import view.TelasEnum;
 
-public class ControllerListarUsuarios {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerListarUsuarios implements Initializable {
 
     @FXML
     private Button buttonAlterarUsuario;
+
+    @FXML
+    private Button buttonAtualizar;
 
     @FXML
     private Button buttonCadastrarUsuario;
@@ -18,8 +33,28 @@ public class ControllerListarUsuarios {
     private Button buttonVoltarPagina;
 
     @FXML
+    private TableView<Usuario> tvUser;
+
+    @FXML
+    private TableColumn<Usuario, Long> cpf;
+
+    @FXML
+    private TableColumn<Usuario, String> login;
+
+    @FXML
+    private TableColumn<Usuario, String> nome;
+
+    @FXML
     void bttnAlterarUsuarioOn(ActionEvent event) {
         ScreenManager.changeScreen(TelasEnum.ALTERAR_USUARIO.name());
+    }
+
+    @FXML
+    void bttnAtualizarOn(ActionEvent event) {
+//        IRepositorioUsuario iRepositorioUsuario = RepositorioUsuario.getInstance();
+//
+//        ObservableList<Usuario> userList = FXCollections.observableArrayList(iRepositorioUsuario.getUsuarios());
+//        tvUser.setItems(userList);
     }
 
     @FXML
@@ -32,4 +67,16 @@ public class ControllerListarUsuarios {
         ScreenManager.changeScreen(TelasEnum.PRINCIPAL_GERENTE.name());
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        IRepositorioUsuario iRepositorioUsuario = RepositorioUsuario.getInstance();
+//
+//        nome.setCellValueFactory(new PropertyValueFactory<Usuario, String >("nome"));
+//        login.setCellValueFactory(new PropertyValueFactory<Usuario, String >("login"));
+//        cpf.setCellValueFactory(new PropertyValueFactory<Usuario, Long >("cpf"));
+//
+//        ObservableList<Usuario> userList = FXCollections.observableArrayList(iRepositorioUsuario.getUsuarios());
+//
+//        tvUser.setItems(userList);
+    }
 }
