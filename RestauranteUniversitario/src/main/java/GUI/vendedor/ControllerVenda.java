945efad1aab2d5070c9e2be9bcba7fd3791e725d
LucasRMacielP <lucasrmacielp@outlook.com>
2023-08-25 13:22:44 -0300

    GUI vendas avulsas

--- RestauranteUniversitario/src/main/java/GUI/vendedor/ControllerVenda.java
+++ RestauranteUniversitario/src/main/java/GUI/vendedor/ControllerVenda.java
@@ -3,6 +3,8 @@ package GUI.vendedor;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import view.ScreenManager;
 import view.TelasEnum;
 
@@ -10,6 +12,84 @@ public class ControllerVenda {
 
     @FXML
     private Button buttonVoltarPagina;
+    private int contadorAlmoco = 0;
+    private int contadorJantar = 0;
+
+
+    @FXML
+    private CheckBox cbAlmoco;
+
+    @FXML
+    private CheckBox cbJantar;
+
+    @FXML
+    private Label labelQuantidadeJantar;
+
+    @FXML
+    private Label labelQuantidadeAlmoco;
+
+    @FXML
+    void bttnDecrementarOn1(ActionEvent event) {
+        if (contadorAlmoco > 0 && cbAlmoco.isSelected()) {
+            contadorAlmoco--;
+            atualizarLabelNum(labelQuantidadeAlmoco, contadorAlmoco);
+        }
+    }
+
+    @FXML
+    void bttnDecrementarOn2(ActionEvent event) {
+        if (contadorJantar > 0 && cbJantar.isSelected()) {
+            contadorJantar--;
+            atualizarLabelNum(labelQuantidadeJantar, contadorJantar);
+        }
+    }
+
+    @FXML
+    void bttnIncrementarOn1(ActionEvent event) {
+        if (cbAlmoco.isSelected()) {
+            contadorAlmoco++;
+            atualizarLabelNum(labelQuantidadeAlmoco, contadorAlmoco);
+        }
+    }
+
+    @FXML
+    void bttnIncrementarOn2(ActionEvent event) {
+        if (cbJantar.isSelected()) {
+            contadorJantar++;
+            atualizarLabelNum(labelQuantidadeJantar, contadorJantar);
+        }
+    }
+
+
+
+
+
+    @FXML
+    void bttnEfetuarCompraOn(ActionEvent event) {
+//        ScreenManager.getInstance().changeScreen(TelasEnum.PAGAMENTO.name());
+//        ScreenManager.getInstance().getControllerPagamento().inicializarValores();
+    }
+
+    private void atualizarLabelNum (Label label, int contador) {
+        label.setText(Integer.toString(contador));
+    }
+
+    public int getContadorAlmoco() {
+        return contadorAlmoco;
+    }
+
+    public int getContadorJantar() {
+        return contadorJantar;
+    }
+
+    public void clearFields(){
+        contadorAlmoco = 0;
+        contadorJantar = 0;
+        atualizarLabelNum(labelQuantidadeAlmoco, 0);
+        atualizarLabelNum(labelQuantidadeJantar, 0);
+        cbAlmoco.setSelected(false);
+        cbJantar.setSelected(false);
+    }
 
     @FXML
     void bttnVoltarPaginaOn(ActionEvent event) {
