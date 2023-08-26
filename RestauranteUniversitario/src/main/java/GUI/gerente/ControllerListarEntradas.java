0a8fbc680b749137882c53d1b5e379f3b1a9c8d3
Higor Matheus <higormatheus1618@gmail.com>
2023-08-26 06:07:35 -0300

    Criada tela de listar entradas

---
+++ RestauranteUniversitario/src/main/java/GUI/gerente/ControllerListarEntradas.java
@@ -0,0 +1,41 @@
+package GUI.gerente;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class ControllerListarEntradas {
+
+    @FXML
+    private Button bttnFiltrar;
+
+    @FXML
+    private Button buttonVoltarPagina;
+
+    @FXML
+    private TableColumn<?, ?> tblColCliente;
+
+    @FXML
+    private TableColumn<?, ?> tblColDataHora;
+
+    @FXML
+    private TableColumn<?, ?> tblColIdCardapio;
+
+    @FXML
+    private TableColumn<?, ?> tblColIdFicha;
+
+    @FXML
+    private TableColumn<?, ?> tblColTipo;
+
+    @FXML
+    void bttnFiltrarOn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bttnVoltarPaginaOn(ActionEvent event) {
+
+    }
+
+}
--- RestauranteUniversitario/src/main/java/GUI/gerente/ControllerPrincipalGerente.java
+++ RestauranteUniversitario/src/main/java/GUI/gerente/ControllerPrincipalGerente.java
@@ -2,25 +2,42 @@ package GUI.gerente;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import view.ScreenManager;
 import view.TelasEnum;
 
 public class ControllerPrincipalGerente {
 
     @FXML
-    void bttnListarCardapios(ActionEvent event) {
+    private Button buttonListarCardapios;
+
+    @FXML
+    private Button buttonListarEntradas;
+
+    @FXML
+    private Button buttonListarRegistros;
+
+    @FXML
+    private Button buttonListarUsuarios;
+
+    @FXML
+    private Hyperlink hlSair;
+
+    @FXML
+    void bttnListarCardapiosOn(ActionEvent event) {
         ScreenManager.getInstance().getControllerListarCardapios().atualizarApresentacao();
         ScreenManager.getInstance().changeScreen(TelasEnum.VISUALIZAR_CARDAPIOS.name());
     }
 
     @FXML
-    void bttnListarRegistrosCompra(ActionEvent event) {
+    void bttnListarRegistrosCompraOn(ActionEvent event) {
         ScreenManager.getInstance().getControllerListarRegistroCompra().atualizarApresentacao();
         ScreenManager.getInstance().changeScreen(TelasEnum.LISTAR_REGISTRO_COMPRA.name());
     }
 
     @FXML
-    void bttnListarUsuarios(ActionEvent event) {
+    void bttnListarUsuariosOn(ActionEvent event) {
         ScreenManager.getInstance().getControllerListarUsuarios().atualizarApresentacao();
         ScreenManager.getInstance().changeScreen(TelasEnum.LISTAR_USUARIO.name());
     }
@@ -28,6 +45,11 @@ public class ControllerPrincipalGerente {
     @FXML
     void hlSairOn(ActionEvent event) {
         ScreenManager.getInstance().changeScreen(TelasEnum.LOGIN.name());
+    }
+
+    @FXML
+    void bttnListarEntradasOn(ActionEvent event) {
+
     }
 
 }
