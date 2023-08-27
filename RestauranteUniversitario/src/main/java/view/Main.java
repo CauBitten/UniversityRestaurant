16b9d0ee6caa6f258a32904b9cbe182803ac7134
Cauã Bittencourt <caua.fb@gmail.com>
2023-08-27 17:30:45 -0300

    If no main

--- RestauranteUniversitario/src/main/java/view/Main.java
+++ RestauranteUniversitario/src/main/java/view/Main.java
@@ -3,25 +3,47 @@ package view;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.Scanner;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Stage secondStage = new Stage();
+        Scanner scanner = new Scanner(System.in);
+        int escolha = 0;
 
-        primaryStage.setScene(ScreenManager.getInstance().getLoginScene());
-        primaryStage.setTitle("RU | LOGIN");
-        primaryStage.setResizable(false);
+        while (escolha != 1 && escolha != 2) {
 
-        secondStage.setScene(ScreenManager.getInstance().getCatracaScene());
-        secondStage.setTitle("RU | CATRACA");
-        secondStage.setResizable(false);
+            System.out.println("Você deseja ir para o app(1) ou simular a catraca(2)?");
+            escolha = scanner.nextInt();
 
-        ScreenManager.getInstance().setPrimaryStage(primaryStage);
-        ScreenManager.getInstance().setSecondStage(secondStage);
+            System.out.println("Opção inválida! Tente novamente!\n");
 
-        primaryStage.show();
-        secondStage.show();
+        }
+
+        if (escolha == 1) {
+
+            primaryStage.setScene(ScreenManager.getInstance().getLoginScene());
+            primaryStage.setTitle("RU | LOGIN");
+            primaryStage.setResizable(false);
+
+            ScreenManager.getInstance().setPrimaryStage(primaryStage);
+
+            primaryStage.show();
+
+        } else if (escolha == 2) {
+
+            Stage secondStage = new Stage();
+
+            secondStage.setScene(ScreenManager.getInstance().getCatracaScene());
+            secondStage.setTitle("RU | CATRACA");
+            secondStage.setResizable(false);
+
+            ScreenManager.getInstance().setSecondStage(secondStage);
+
+            secondStage.show();
+
+        }
     }
 
     public static void main(String[] args) {
